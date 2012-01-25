754da6d043c67046b2e127183b5f4d838e3daf28
Addy Osmani <addyosmani@gmail.com>
2012-01-25 12:22:37 +0000

    Fixes #3

--- new-version/js/main.js
+++ new-version/js/main.js
@@ -4,7 +4,6 @@ require.config({
     jquery: 'libs/jquery/jquery-min',
     underscore: 'libs/underscore/underscore-min',
     backbone: 'libs/backbone/backbone-optamd3-min',
-    tmpl: 'libs/tmpl/jquery.tmpl',
     text: 'libs/require/text'
   }
 
