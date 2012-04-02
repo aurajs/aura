963aa247321c07d2e4443fdb3c82253a9b988ce1
Dustin Boston <dustin@dblogit.com>
2012-04-01 23:52:43 -0700

    Fixing commented code, introducted circular dependency

--- aura-example/js/collections/todos.js
+++ aura-example/js/collections/todos.js
@@ -1,42 +1,34 @@
-define({});
+define(["../extensions/facade"], function (facade) {
 
-// define([
-//   'underscore', 
-//   'backbone', 
-//   'libs/backbone/localstorage', 
-//   'models/todo'
-//   ], function(_, Backbone, Store, Todo){
-	  
-// 	var TodosCollection = Backbone.Collection.extend({
+    var TodosCollection = facade.backbone.Collection.extend({
 
-//     // Reference to this collection's model.
-//     model: Todo,
+        // Reference to this collection's model.
+        model: facade.models.todo,
 
-//     // Save all of the todo items under the `"todos"` namespace.
-//     localStorage: new Store("todos"),
+        // Save all of the todo items under the `"todos"` namespace.
+        localStorage: new facade.data.store("todos"),
 
-//     // Filter down the list of all todo items that are finished.
-//     done: function() {
-//       return this.filter(function(todo){ return todo.get('done'); });
-//     },
+        // Filter down the list of all todo items that are finished.
+        done: function() {
+            return this.filter(function(todo){ return todo.get('done'); });
+        },
 
-//     // Filter down the list to only todo items that are still not finished.
-//     remaining: function() {
-//       return this.without.apply(this, this.done());
-//     },
+        // Filter down the list to only todo items that are still not finished.
+        remaining: function() {
+            return this.without.apply(this, this.done());
+        },
 
-//     // We keep the Todos in sequential order, despite being saved by unordered
-//     // GUID in the database. This generates the next order number for new items.
-//     nextOrder: function() {
-//       if (!this.length) return 1;
-//       return this.last().get('order') + 1;
-//     },
+        // We keep the Todos in sequential order, despite being saved by unordered
+        // GUID in the database. This generates the next order number for new items.
+        nextOrder: function() {
+           if (!this.length) return 1;
+           return this.last().get('order') + 1;
+        },
 
-//     // Todos are sorted by their original insertion order.
-//     comparator: function(todo) {
-//       return todo.get('order');
-//     }
-
-//   });
-//   return new TodosCollection;
-// });
+        // Todos are sorted by their original insertion order.
+        comparator: function(todo) {
+            return todo.get('order');
+        }
+    });
+    return new TodosCollection();
+});
