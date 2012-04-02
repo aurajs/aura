6f2d52f6e45f45e45ff2f1332007e8a465f4e5d2
Dustin Boston <dustin@dblogit.com>
2012-04-01 23:56:18 -0700

    Updated a few methods to use the facade

--- aura-example/js/modules/app_init.js
+++ aura-example/js/modules/app_init.js
@@ -22,59 +22,61 @@ define(["../extensions/facade", "text!../templates/stats.html"],
             // collection, when items are added or changed. Kick things off by
             // loading any preexisting todos that might be saved in *localStorage*.
             initialize: function() {
-                // _.bindAll(this, 'addOne', 'addAll', 'render');
+                facade.events.bindAll(this, 'addOne', 'addAll', 'render');
 
-                // this.input    = this.$("#new-todo");
+                this.input    = facade.dom.find("#new-todo", this.el);
 
-                // Todos.bind('add',     this.addOne);
-                // Todos.bind('reset',   this.addAll);
-                // Todos.bind('all',     this.render);
+                facade.collections.todos.bind('add',     this.addOne);
+                facade.collections.todos.bind('reset',   this.addAll);
+                facade.collections.todos.bind('all',     this.render);
 
-                // Todos.fetch();
+                facade.collections.todos.fetch();
             },
 
             // Re-rendering the App just means refreshing the statistics -- the rest
             // of the app doesn't change.
             render: function() {
-                // mediator.publish('renderDone', this, Todos);
+                facade.publish('renderDone', this, facade.collections.todos);
             },
 
             // Add a single todo item to the list by creating a view for it, and
             // appending its element to the `<ul>`.
+            // Should just publish an event that lets the todo view handle the display
             addOne: function(todo) {
                 // var view = new TodoView({model: todo});
                 // this.$("#todo-list").append(view.render().el);
+                facade.publish('newTodo', this, todo);
             },
 
             // Add all items in the **Todos** collection at once.
             addAll: function() {
-                // Todos.each(this.addOne);
+                facade.collections.todos.each(this.addOne);
             },
 
             // Generate the attributes for a new Todo item.
             newAttributes: function() {
-                // return {
-                //     content: this.input.val(),
-                //     order:   Todos.nextOrder(),
-                //     done:    false
-                // };
+                return {
+                    content: this.input.val(),
+                    order:   facade.collections.todos.nextOrder(),
+                    done:    false
+                };
             },
 
             // If you hit return in the main input field, create new **Todo** model,
             // persisting it to *localStorage*.
             createOnEnter: function(e) {
-                // mediator.publish('createWhenEntered', this, e, Todos);
+                facade.publish('createWhenEntered', this, e, facade.collections.todos);
                 console.log("createOnEnter triggered, publish event");
             },
 
             // Clear all done todo items, destroying their models.
             clearCompleted: function() {
-                // mediator.publish('clearContent', Todos);
-                // return false;
+                facade.publish('clearContent', facade.collections.todos);
+                return false;
             },
 
             addEntry:function(){
-                // mediator.publish('addingNewTodo', this);
+                facade.publish('addingNewTodo', this);
                 console.log("addEntry triggered, publish event");
             }
 
