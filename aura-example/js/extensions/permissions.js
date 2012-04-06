eb10cf3ea093a57d14b7831e609835f845e2c81e
Dustin Boston <dustin@dblogit.com>
2012-04-06 00:33:23 -0700

    Added permissions for new modules

--- aura-example/js/extensions/permissions.js
+++ aura-example/js/extensions/permissions.js
@@ -2,6 +2,9 @@
  * @fileOverview Extend the aura-core permissions
  */
 define(["../libs/aura-core/permissions"], function (permissions) {
+    
     permissions.rules.appInit = {bootstrap:true};
+	permissions.rules.newTodo = {bootstrap:true};
+    
     return permissions;
 });
