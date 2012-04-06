8d65df0f7e6b3f4fa109594e1c1759d0e15f8c30
Dustin Boston <dustin@dblogit.com>
2012-04-06 00:33:03 -0700

    Removed collections and models

--- aura-example/js/extensions/mediator.js
+++ aura-example/js/extensions/mediator.js
@@ -4,74 +4,56 @@
 /*jslint sloppy:true*/
 /*global define*/
 define(["../libs/aura-core/mediator", "backbone", "../libs/backbone/localstorage"], 
-    function (mediator, backbone, Store) {
+    function (mediator, Backbone, Store) {
 
-    var Todos, Todo;
+        var initialized = {
+            collections: {},
+            models: {}
+        };
 
-    Todo = backbone.Model.extend({
+        mediator.mvc = {
+            createView: function (view) {
+                return Backbone.View.extend(view);
+            },
+            createCollection: function (collection) {
+                return Backbone.Collection.extend(collection);
+            },
+            createModel: function (model) {
+                return Backbone.Model.extend(model);
+            },
+            getCollection: function (collection) {
+                var dfr;
+                if (initialized.collections[collection] !== undefined) {
+                    return initialized.collections[collection];
+                } else {
+                    dfr = mediator.data.deferred();
+                    // @todo error handling
+                    require(["collections/" + collection], function (Module) {
+                        initialized.collections[collection] = new Module();
+                        dfr.resolve(initialized.collections[collection]);
+                    });
+                    
+                    return dfr.promise();
+                }
+            },
+            getModel: function (model) {
+                var dfr;
+                if (initialized.models[model] !== undefined) {
+                    return initialized.models[model];
+                } else {
+                    dfr = mediator.data.deferred();
+                    // @todo error handling
+                    require(["models/" + model], function (Module) {
+                        initialized.models[model] = new Module();
+                        dfr.resolve(initialized.models[model]);
+                    });
+                    
+                    return dfr.promise();
+                }
+            }
+        };
 
-     // Default attributes for the todo.
-     defaults: {
-       content: "empty todo...",
-       done: false
-     },
-
-     // Ensure that each todo created has `content`.
-     initialize: function() {
-       if (!this.get("content")) {
-         this.set({"content": this.defaults.content});
-       }
-     },
-
-     // Toggle the `done` state of this todo item.
-     toggle: function() {
-       this.save({done: !this.get("done")});
-     },
-
-     // Remove this Todo from *localStorage* and delete its view.
-     clear: function() {
-       this.destroy();
-       this.view.remove();
-     }
-
-   });
-
-    Todos = backbone.Collection.extend({
-
-        // Reference to this collection's model.
-        model: Todo,
-
-        // Save all of the todo items under the `"todos"` namespace.
-        localStorage: new Store("todos"),
-
-        // Filter down the list of all todo items that are finished.
-        done: function() {
-            return this.filter(function(todo){ return todo.get('done'); });
-        },
-
-        // Filter down the list to only todo items that are still not finished.
-        remaining: function() {
-            return this.without.apply(this, this.done());
-        },
-
-        // We keep the Todos in sequential order, despite being saved by unordered
-        // GUID in the database. This generates the next order number for new items.
-        nextOrder: function() {
-           if (!this.length) return 1;
-           return this.last().get('order') + 1;
-        },
-
-        // Todos are sorted by their original insertion order.
-        comparator: function(todo) {
-            return todo.get('order');
-        }
-    });
-
-
-        mediator.data.store = Store;
-        mediator.backbone = backbone;
-        mediator.models = {todo: Todo};
-        mediator.collections = {todos: new Todos};
+        mediator.data.Store = Store;
 
         return mediator;
      });
