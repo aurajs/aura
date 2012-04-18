c4ac687476b143b4e7a2f847fda46ed497557efb
Dustin Boston <dustin@dblogit.com>
2012-04-18 14:28:08 -0700

    Removed aura-core to prevent confusion until the example is actually working.

--- aura-core/js/facade.js
+++
@@ -1,8 +0,0 @@
-/**
- * Extends the core facade
- */
-/*global define*/
-define(["./core/mediator", "./core/facade", "./permissions"], function (mediator, facade, permissions) {
-	facade.events.queue = mediator.events.queue;
-	return facade;
-});
