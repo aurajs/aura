24c5bc523eccc65d022d56889f288d3fb92fc7f2
Gerson Goulart <gerson.goulart@gmail.com>
2012-07-11 17:40:58 -0700

    Fix grunt file for a successful build. Also fix mixed spaces and tabs in root files and add a .editorconfig file to help the community to keep the coding style.

--- grunt.js
+++ grunt.js
@@ -1,118 +1,124 @@
-/*global module:false*/
+/* global module:false */
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-contrib');
-  grunt.loadNpmTasks('grunt-requirejs');
-  
-  // Project configuration.
-  grunt.initConfig({
 
-    // MULTI TASKS
-    // -----------
+	grunt.loadNpmTasks('grunt-contrib');
+	grunt.loadNpmTasks('grunt-requirejs');
 
-    // clean build directory
-    clean: ['demo-build'],
+	// Project configuration.
+	grunt.initConfig({
 
-    // js linting
-    lint: {
-      files: [
-        'demo/js/widgets/calendar/collections/*.js',
-        'demo/js/widgets/calendar/models/*.js',
-        'demo/js/widgets/calendar/views/*.js',                
-        'demo/js/widgets/todos/collections/*.js',
-        'demo/js/widgets/todos/models/*.js',
-        'demo/js/widgets/todos/views/*.js', 
-        'demo/js/widgets/controls/**/*.js',
-        'demo/js/widgets/**/main.js',             
-        'demo/js/app.js'
-      ],
-    },
+		// MULTI TASKS
+		// -----------
 
-    // qunit testsuites
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    
-    // tasks to be executed and files
-    // to be watched for changes
-    watch: {
-      files: ['<config:lint.files>'],
-      tasks: ['lint qunit']
-    },
+		// clean build directory
+		clean: ['demo-build'],
 
-    // SINGLE TASKS
-    // ----------------------
+		// js linting
+		lint: {
+			files: [
+				'demo/js/widgets/calendar/collections/*.js',
+				'demo/js/widgets/calendar/models/*.js',
+				'demo/js/widgets/calendar/views/*.js',
+				'demo/js/widgets/todos/collections/*.js',
+				'demo/js/widgets/todos/models/*.js',
+				'demo/js/widgets/todos/views/*.js',
+				'demo/js/widgets/controls/**/*.js',
+				'demo/js/widgets/**/main.js',
+				'demo/js/app.js'
+			]
+		},
 
-    // require js
-    requirejs: {
-        // build directory path
-        dir: 'demo-build',
-        // applicatioon directory
-        appDir: 'demo',
-        // base url for retrieving paths
-        baseUrl: 'js',
-        // shim underscore & backbone (cause we use the non AMD versions here)
-  	    shim: {
-  	        'underscore': {
-  	            exports: '_'
-  	        },
-  	        'backbone': {
-  	            deps: ['underscore', 'jquery'],
-  	            exports: 'Backbone'
-  	        }
-  	    },
-        // paths
-        paths: {
+		// qunit testsuites
+		qunit: {
+			files: ['test/**/*.html']
+		},
 
-            core: 'ext/mediator',
-            perms: 'ext/permissions',
-            sandbox: 'ext/facade',
+		// tasks to be executed and files
+		// to be watched for changes
+		watch: {
+			files: ['<config:lint.files>'],
+			tasks: ['lint qunit']
+		},
 
-            aura_core: '../../aura/mediator',
-            aura_perms: '../../aura/permissions',
-            aura_sandbox: '../../aura/facade',
+		// SINGLE TASKS
+		// ----------------------
 
-            text: 'ext/lib/text',
-            backbone: 'ext/lib/backbone',
-            localstorage: 'ext/lib/localstorage',
-            jquery: '../../aura/lib/jquery',
-            underscore: '../../aura/lib/underscore',
-            fullcalendar: 'ext/lib/fullcalendar.min',
-            jquery_ui: 'ext/lib/jquery-ui.min'
-        },
-        // optimize javascript files with uglifyjs
-        optimize: 'uglify',
-        // define our app model
-        modules: [{name: 'app'}]
-    },
+		// require js
+		requirejs: {
+			// build directory path
+			dir: 'demo-build',
+			// applicatioon directory
+			appDir: 'demo',
+			// base url for retrieving paths
+			baseUrl: 'js',
+			// shim underscore & backbone (cause we use the non AMD versions here)
+			shim: {
+				'underscore': {
+					exports: '_'
+				},
+				'backbone': {
+					deps: ['underscore', 'dom'],
+					exports: 'Backbone'
+				},
+				'deferred':{
+					deps: ['dom']
+				}
+			},
+			// paths
+			paths: {
+				// jQuery
+				dom: '../../aura/lib/jquery/jquery',
 
-    // Configuration
-    // -------------
+				core: '../../backbone-aura/mediator',
+				perms: '../../backbone-aura/permissions',
+				sandbox: '../../backbone-aura/facade',
 
-    // js linting options
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        eqnull: true,
-        browser: true,        
-        nomen: false
-      },
-      globals: {
-        require: true,
-        define: true,
-        $: true
-      }
-    }
+				aura_core: '../../aura/mediator',
+				aura_perms: '../../aura/permissions',
+				aura_sandbox: '../../aura/facade',
 
-  });
+				text: '../../backbone-aura/lib/text',
+				backbone: '../../backbone-aura/lib/backbone',
+				localstorage: '../../backbone-aura/lib/localstorage',
+				jquery: '../../aura/lib/jquery',
+				underscore: '../../aura/lib/underscore',
+				fullcalendar: '../../backbone-aura/lib/fullcalendar.min',
+				jquery_ui: '../../backbone-aura/lib/jquery-ui.min'
+			},
+			// optimize javascript files with uglifyjs
+			optimize: 'uglify',
+			// define our app model
+			modules: [{ name: 'app' }]
+		},
 
-  // build task
-  grunt.registerTask('build', 'clean lint requirejs');
+		// Configuration
+		// -------------
+
+		// js linting options
+		jshint: {
+			options: {
+				curly: true,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				eqnull: true,
+				browser: true,
+				nomen: false
+			},
+			globals: {
+				require: true,
+				define: true,
+				$: true
+			}
+		}
+
+	});
+
+	// build task
+	grunt.registerTask('build', 'clean lint requirejs');
 
 };
