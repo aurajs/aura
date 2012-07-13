fa2542029efa7588d7ae77adc2a9f7eecb890e3d
Peter Rudolfsen <peter@silencethefoe.com>
2012-07-13 02:18:18 +0200

    Moved shim/path config to its own file.
    Config-loading in the optimized version should be improved
    as it now loads it even though it's not needed.

--- grunt.js
+++ grunt.js
@@ -50,52 +50,9 @@ module.exports = function(grunt) {
 			dir: 'demo-build',
 			// applicatioon directory
 			appDir: 'demo',
+			mainConfigFile: 'config.js',
 			// base url for retrieving paths
 			baseUrl: 'js',
-			// shim underscore & backbone (cause we use the non AMD versions here)
-			shim: {
-				'dom': {
-					exports: '$',
-					deps: ['jquery']
-				},
-				'underscore': {
-					exports: '_'
-				},
-				'backbone': {
-					deps: ['underscore', 'dom'],
-					exports: 'Backbone'
-				},
-				'deferred': {
-					exports: 'Deferred',
-					deps: ['dom']
-				}
-			},
-			// paths
-			paths: {
-				// jQuery
-				jquery: '../../aura/lib/jquery/jquery',
-
-				// Zepto
-				zepto: '../../aura/lib/zepto/zepto',
-				deferred: '../../aura/lib/zepto/deferred',
-
-				dom: '../../aura/lib/dom',
-
-				core: '../../backbone-aura/mediator',
-				perms: '../../backbone-aura/permissions',
-				sandbox: '../../backbone-aura/facade',
-
-				aura_core: '../../aura/mediator',
-				aura_perms: '../../aura/permissions',
-				aura_sandbox: '../../aura/facade',
-
-				text: '../../backbone-aura/lib/text',
-				backbone: '../../backbone-aura/lib/backbone',
-				localstorage: '../../backbone-aura/lib/localstorage',
-				underscore: '../../aura/lib/underscore',
-				fullcalendar: '../../backbone-aura/lib/fullcalendar.min',
-				jquery_ui: '../../backbone-aura/lib/jquery-ui.min'
-			},
 			// optimize javascript files with uglifyjs
 			optimize: 'uglify',
 			// define our app model
