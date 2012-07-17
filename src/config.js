ff4405e519c0cac798416bb0d37ba10816b0a9eb
Gerson Goulart <gerson.goulart@gmail.com>
2012-07-17 09:57:59 -0700

    Move permissions file from extensions/backbone to apps/demo and adjust config.js accordingly.

--- src/config.js
+++ src/config.js
@@ -1,46 +1,48 @@
 require.config({
-    // shim underscore & backbone (cause we use the non AMD versions here)
-    shim: {
-        'dom': {
-            exports: '$',
-            deps: ['jquery'] // switch to the DOM-lib of your choice
-        },
-        'underscore': {
-            exports: '_'
-        },
-        'backbone': {
-            deps: ['underscore', 'dom'],
-            exports: 'Backbone'
-        },
-        'deferred': {
-            exports: 'Deferred',
-            deps: ['dom']
-        }
-    },
-    // paths
-    paths: {
-        // jQuery
-        jquery: '../../../aura/lib/jquery/jquery',
+	// shim underscore & backbone (cause we use the non AMD versions here)
+	shim: {
+		'dom': {
+			exports: '$',
+			deps: ['jquery'] // switch to the DOM-lib of your choice
+		},
+		'underscore': {
+			exports: '_'
+		},
+		'backbone': {
+			deps: ['underscore', 'dom'],
+			exports: 'Backbone'
+		},
+		'deferred': {
+			exports: 'Deferred',
+			deps: ['dom']
+		}
+	},
+	// paths
+	paths: {
+		// jQuery
+		jquery: '../../../aura/lib/jquery/jquery',
 
-        // Zepto
-        zepto: '../../../aura/lib/zepto/zepto',
-        deferred: '../../../aura/lib/zepto/deferred',
+		// Zepto
+		zepto: '../../../aura/lib/zepto/zepto',
+		deferred: '../../../aura/lib/zepto/deferred',
 
-        dom: '../../../aura/lib/dom',
+		// Aura
+		dom: '../../../aura/lib/dom',
+		underscore: '../../../aura/lib/underscore',
+		aura_core: '../../../aura/mediator',
+		aura_perms: '../../../aura/permissions',
+		aura_sandbox: '../../../aura/facade',
 
-        core: '../../../extensions/backbone/mediator',
-        perms: '../../../extensions/backbone/permissions',
-        sandbox: '../../../extensions/backbone/facade',
+		// Backbone Extension
+		core: '../../../extensions/backbone/mediator',
+		sandbox: '../../../extensions/backbone/facade',
+		text: '../../../extensions/backbone/lib/text',
+		backbone: '../../../extensions/backbone/lib/backbone',
+		localstorage: '../../../extensions/backbone/lib/localstorage',
+		fullcalendar: '../../../extensions/backbone/lib/fullcalendar.min',
+		jquery_ui: '../../../extensions/backbone/lib/jquery-ui.min',
 
-        aura_core: '../../../aura/mediator',
-        aura_perms: '../../../aura/permissions',
-        aura_sandbox: '../../../aura/facade',
-
-        text: '../../../extensions/backbone/lib/text',
-        backbone: '../../../extensions/backbone/lib/backbone',
-        localstorage: '../../../extensions/backbone/lib/localstorage',
-        underscore: '../../../aura/lib/underscore',
-        fullcalendar: '../../../extensions/backbone/lib/fullcalendar.min',
-        jquery_ui: '../../../extensions/backbone/lib/jquery-ui.min'
-    }
+		// Demo App
+		perms: '../../../apps/demo/js/permissions'
+	}
 });
