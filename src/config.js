8f5f87f3a27130c5e16fb24f6dacd26ced472202
Gerson Goulart <gerson.goulart@gmail.com>
2012-10-18 23:10:51 -0700

    Update src/config.js
    After your question I just realized I could do the same thing in a better, more clean way.

--- src/config.js
+++ src/config.js
@@ -1,99 +1,97 @@
-(function(){
+// Define call just to make enforceDefine check happy
+define(function() {
 
   'use strict';
 
   // Aura configuration object is separate from require.config so we can have
   // access to it in src/aura/base.js
-  var ra = require.aura = {};
+  require.aura = {
 
-  // switch to the DOM-lib of your choice
-  // Could be either 'jquery' or 'zepto';
-  ra.domLibrary = 'jquery';
+    // [RequireJS](http://requirejs.org/) 2.0+ plus has error callbacks (errbacks)
+    // which provide per-require error handling. To utilize this feature
+    // enforceDefine must be enabled and non-AMD dependencies must be shimmed.
+    enforceDefine: true,
 
-  // [RequireJS](http://requirejs.org/) 2.0+ plus has error callbacks (errbacks)
-  // which provide per-require error handling. To utilize this feature
-  // enforceDefine must be enabled and non-AMD dependencies must be shimmed.
-  ra.enforceDefine= true;
+    // Override the config for the i18n module ID
+    // Uncomment to load the french i18n bundle for widgets
+    // locale: 'fr-fr',
 
-  // Override the config for the i18n module ID
-  // Uncomment to load the french i18n bundle for widgets
-  // ra.locale = 'fr-fr';
+    baseUrl: 'apps/demo/js',
 
-  ra.baseUrl = 'apps/demo/js';
+    // Uncomment if you would like to support cache busting
+    // urlArgs: "bust=v2",
 
-  // Uncomment if you would like to support cache busting
-  // ra.urlArgs = "bust=v2";
-
-  ra.deps = ['app'],
+    deps: ['app'],
 
     // shim underscore(lodash) & backbone (cause we use the non AMD versions here)
-  ra.shim = {
-    'dom': {
-      exports: '$',
-      deps: [ ra.domLibrary ]
+    shim: {
+      'dom': {
+        exports: '$',
+        // switch to the DOM-lib of your choice
+        // Could be either 'jquery' or 'zepto';
+        deps: [ 'jquery' ]
+      },
+      'underscore': {
+        exports: '_'
+      },
+      'backbone': {
+        deps: ['underscore', 'dom'],
+        exports: 'Backbone'
+      },
+      'zepto': {
+        exports: 'Zepto'
+      },
+      'deferred': {
+        exports: 'Deferred',
+        deps: ['dom']
+      },
+      'fullcalendar': {
+        deps: ['jquery'],
+        exports: '$.fullCalendar'
+      },
+      'jquery_ui': {
+        deps: ['jquery'],
+        exports: '$.ui'
+      }
     },
-    'underscore': {
-      exports: '_'
-    },
-    'backbone': {
-      deps: ['underscore', 'dom'],
-      exports: 'Backbone'
-    },
-    'zepto': {
-      exports: 'Zepto'
-    },
-    'deferred': {
-      exports: 'Deferred',
-      deps: ['dom']
-    },
-    'fullcalendar': {
-      deps: ['jquery'],
-      exports: '$.fullCalendar'
-    },
-    'jquery_ui': {
-      deps: ['jquery'],
-      exports: '$.ui'
+
+    // paths
+    paths: {
+      // jQuery
+      jquery: '../../../aura/lib/jquery/jquery',
+
+      // Zepto
+      zepto: '../../../aura/lib/zepto/zepto',
+      deferred: '../../../aura/lib/zepto/deferred',
+
+      // Underscore (Lo-Dash - http://lodash.com)
+      underscore: '../../../aura/lib/lodash',
+
+      // Set the base library
+      dom: '../../../aura/lib/dom',
+
+      // Aura
+      aura_base: '../../../aura/base',
+      aura_core: '../../../aura/core',
+      aura_perms: '../../../aura/permissions',
+      aura_sandbox: '../../../aura/sandbox',
+
+      // Backbone Extension
+      core: '../../../extensions/backbone/core',
+      sandbox: '../../../extensions/backbone/sandbox',
+      text: '../../../extensions/backbone/lib/text',
+      backbone: '../../../extensions/backbone/lib/backbone',
+      i18n: '../../../i18n',
+      localstorage: '../../../extensions/backbone/lib/localstorage',
+      fullcalendar: '../../../extensions/backbone/lib/fullcalendar.min',
+      jquery_ui: '../../../extensions/backbone/lib/jquery-ui.min',
+
+      // Demo App
+      perms: '../../../apps/demo/js/permissions'
     }
+
   };
 
-  // paths
-  ra.paths = {
-    // jQuery
-    jquery: '../../../aura/lib/jquery/jquery',
+  require.config( require.aura );
 
-    // Zepto
-    zepto: '../../../aura/lib/zepto/zepto',
-    deferred: '../../../aura/lib/zepto/deferred',
-
-    // Underscore (Lo-Dash - http://lodash.com)
-    underscore: '../../../aura/lib/lodash',
-
-    // Set the base library
-    dom: '../../../aura/lib/dom',
-
-    // Aura
-    aura_base: '../../../aura/base',
-    aura_core: '../../../aura/core',
-    aura_perms: '../../../aura/permissions',
-    aura_sandbox: '../../../aura/sandbox',
-
-    // Backbone Extension
-    core: '../../../extensions/backbone/core',
-    sandbox: '../../../extensions/backbone/sandbox',
-    text: '../../../extensions/backbone/lib/text',
-    backbone: '../../../extensions/backbone/lib/backbone',
-    i18n: '../../../i18n',
-    localstorage: '../../../extensions/backbone/lib/localstorage',
-    fullcalendar: '../../../extensions/backbone/lib/fullcalendar.min',
-    jquery_ui: '../../../extensions/backbone/lib/jquery-ui.min',
-
-    // Demo App
-    perms: '../../../apps/demo/js/permissions'
-  };
-
-  require.config( ra );
-
-  // Define call just to make enforceDefine check happy
-  define(function() {});
-
-})();
+});
