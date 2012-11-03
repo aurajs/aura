81226b512a6b08e037ca1f9f9bc5c0f61c755702
Sindre Sorhus <sindresorhus@gmail.com>
2012-11-03 15:22:32 +0100

    Add norwegian translation

---
+++ src/nls/nb/todos.js
@@ -0,0 +1,10 @@
+define({
+	"placeholderText": "Hva vil du gjøre?",
+	"markAsComplete": "Merk alle som fullført",
+	"clear": "Tøm",
+	"completedSingular": "fullført",
+	"completedPlural": "fullførte",
+	"itemsLeft": "igjen",
+	"itemSingular": "gjøremål",
+	"itemPlural": "gjøremål"
+});
