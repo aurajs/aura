43a2aff34a005ab383a1b359ffa8880587e00bcb
Jimmy Oliger <hi@jimmyoliger.com>
2013-02-18 18:15:57 +0100

    Add start and stop method in widget

--- spec/lib/ext/widgets_spec.js
+++ spec/lib/ext/widgets_spec.js
@@ -155,6 +155,7 @@ define(['aura/aura', 'aura/ext/widgets'], function (aura, ext) {
       it('Should be possible to nest widgets...');
       // Nesting means that if a widget's markup contains data-aura-widget elements,
       // They should be started recursively
+
     });
 
     describe('Adding new widgets source locations...', function () {
@@ -204,5 +205,54 @@ define(['aura/aura', 'aura/ext/widgets'], function (aura, ext) {
       });
     });
 
+    describe('sandbox', function () {
+      var app;
+      var sandbox;
+      var mediator;
+      before(function (done) {
+        app = aura();
+        //app.use(ext);
+        app.start().done(function () {
+          mediator = app.core.mediator;
+          sandbox = app.createSandbox();
+
+          setTimeout(done, 0);
+        });
+      });
+
+      describe('#start', function () {
+        it('should augment sandbox', function () {
+          sandbox.start.should.be.a('function');
+        });
+
+        var spy;
+        before(function () {
+          spy = sinon.spy();
+          mediator.on('aura.sandbox.start', spy);
+          sandbox.start();
+        });
+
+        it('should emit aura.sandbox.start', function() {
+          spy.should.have.been.called;
+        });
+      });
+
+      describe('#stop', function () {
+        it('should augment sandbox', function () {
+          sandbox.stop.should.be.a('function');
+        });
+
+        var spy;
+        before(function () {
+          spy = sinon.spy();
+          mediator.on('aura.sandbox.stop', spy);
+          sandbox.stop();
+        });
+
+        it('should emit aura.sandbox.stop', function () {
+          spy.should.have.been.called;
+        });
+      });
+    });
   });
 });
